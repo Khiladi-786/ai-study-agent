5a4282c9e12934310e4bec49229953f833f6ee09
Khiladi-786 <nikhilmore1325@gmail.com>
2026-03-16 19:39:41 +0530

    enable CORS for frontend

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 from google import genai
 import os
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
